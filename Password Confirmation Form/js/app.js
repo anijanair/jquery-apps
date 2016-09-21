347714eb6583a923027e30ea3ea74db327b5c2fb
Anija <anija.nair@yahoo.com>
2016-09-21 10:09:46 -0400

    password confirmn. without modified 'submit' button

--- Password Confirmation Form/js/app.js
+++ Password Confirmation Form/js/app.js
@@ -1,24 +1,23 @@
 // To show password hint if pw<9
 // to hide password hint if pw>8
 
-$("#form span").hide();
-
+$("form span").hide();
 function passwordEvent(){
-  if(($this).val().length >8) ; {
-    (this).next().hide();
+  if($(this).val().length >8) {
+    $(this).next().hide();
   }else {
     $(this).next().show();
   }
 }
 
+
+
+$("#password").focus(passwordEvent).keyup(passwordEvent);
+$("#confirm_password").focus(confirmPasswordEvent).keyup(confirmPasswordEvent);
 function confirmPasswordEvent(){
-  $("#password").val()===$("#confirm_password").val(){
-    (this).next().hide();
+  if ($("#password").val()===$("#confirm_password").val()){
+    $(this).next().hide();
   }else {
     $(this).next().show();
   }
-
-  }
 };
-$("#password").focus(passwordEvent).keyup(passwordEvent);
-$("#confirm_password").focus(confirmPasswordEvent).keyup(confirmPasswordEvent);
